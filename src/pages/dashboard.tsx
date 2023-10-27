b14b26a52c1c596cf020a02f781248607bdf4908
joshua ogunwole <joshuaogunwolejo888@gmail.com>
2023-10-27 04:44:45 +0100

    update the application with a modal for adding categories and todo list

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,11 +2,14 @@ import { FC, Fragment, useMemo } from "react";
 import Header from "../common/header/header";
 import Card1 from "../common/cards/card1";
 import Card2 from "../common/cards/card2";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard:FC = () => {
     const categoryFolder = useMemo(() => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
   </svg>, [])
+
+  const navigate = useNavigate()
   
     return (
         <Fragment>
@@ -31,11 +34,11 @@ const Dashboard:FC = () => {
                     <div className="">
                         <div className="grid md:grid-cols-2 lg:grid-cols-3 lg:gap-4">
                             <div className="">
-                                <Card2 cardTitle="Todos" list={[]} routerHandler={() => null}/>
+                                <Card2 cardTitle="Todos" list={[]} routerHandler={() => navigate("/my-todo")}/>
                             </div>
 
                             <div className="">
-                                <Card2 cardTitle="Category" list={[]} routerHandler={() => null} listIcon={categoryFolder}/>
+                                <Card2 cardTitle="Category" list={[]} routerHandler={() => navigate("/categories")} listIcon={categoryFolder}/>
                             </div>
 
                             <div className="">
--- src/pages/todo.list.tsx
+++ src/pages/todo.list.tsx
@@ -1,9 +1,10 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import Header from "../common/header/header";
 import SaveButton from "../common/button/save.button";
 import Card1 from "../common/cards/card1";
 import List from "../component/list.items";
 import TodoListComponent from "../component/todo.list";
+import { AddModal } from "../common/modal/add.modal";
 
 const defaultIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#000" className="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
@@ -12,6 +13,14 @@ const defaultIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox=
 
 const TodoList:FC = () => {
     const a:Array<number> = [1,2,3,4,5,6,7,8]
+
+    // adding a new category
+    const [addNewCategory, setAddNewCategory] = useState<boolean>(false)
+    // opening the new category modal
+    const openHandler = () => {
+        setAddNewCategory(true)
+    }
+    
     return (
         <Fragment>
             <Header title="My Todo List"/>
@@ -53,7 +62,7 @@ const TodoList:FC = () => {
                         </div>
                         
                         <div className="flex flex-row justify-end items-center mt-5 py-3 mr-5 w-full">
-                            <button>
+                            <button onClick={openHandler}>
                                 <div>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" className="w-8 h-8">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -65,6 +74,7 @@ const TodoList:FC = () => {
                     
                 </div>
             </div>
+            <AddModal close={() => setAddNewCategory(false)} open={addNewCategory}/>
         </Fragment>
     )
 }

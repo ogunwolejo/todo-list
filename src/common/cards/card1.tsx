c6629792a52cae3ab8ef0e913ef00067375a19aa
joshua ogunwole <joshuaogunwolejo888@gmail.com>
2023-10-28 02:32:22 +0100

    changing the font size of card 1

--- src/common/cards/card1.tsx
+++ src/common/cards/card1.tsx
@@ -5,8 +5,8 @@ const Card1:FC<{title:string; bgColor:string; data:number; color?:string }> = ({
         <Fragment>
             <div className={`h-32 md:h-40 w-full border-box shadow-xl border border-transparent ${bgColor} rounded-lg flex flex-row items-center justify-center hover:cursor-pointer`}>
                 <div className="flex flex-col items-center p-2">
-                    <div className={`text-lg md:text-2xl font-extrabold self-center ${color}`}>{data}</div>
-                    <label className={`text-xs md:text-md my-1 font-semibold font-poppins mx-3 w-full text-center ${color}`}>{title}</label>
+                    <div className={`text-lg lg:text-xl xl:text-2xl font-extrabold self-center ${color}`}>{data}</div>
+                    <label className={`text-xs md:text-sm lg:text-md xl:text-lg my-1 font-semibold font-poppins mx-3 w-full text-center ${color}`}>{title}</label>
                 </div>
             </div>
         </Fragment>
